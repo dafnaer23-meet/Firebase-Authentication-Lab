ab28e314cbc193910aac1079e87298c9528f1c3e
dafnaer23-meet <109908294+dafnaer23-meet@users.noreply.github.com>
2022-08-01 13:04:54 +0300

    uihuinhh

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -2,19 +2,32 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask import session as login_session
 import pyrebase
 
-app = Flask(__name__, template_folder='templates', static_folder='static')
-app.config['SECRET_KEY'] = 'super-secret-key'
+
 
 firebaseConfig = {
-  apiKey: "AIzaSyBSqeUlc81M5UGbfSLWnCH1pxSiHPjp9Qw",
-  authDomain: "bjhbjhb-63bfd.firebaseapp.com",
-  projectId: "bjhbjhb-63bfd",
-  storageBucket: "bjhbjhb-63bfd.appspot.com",
-  messagingSenderId: "985216799062",
-  appId: "1:985216799062:web:b62627a0d0a67a1f0806f7",
-  measurementId: "G-N4B71HJVBH"
+
+  "apiKey": "AIzaSyCcVltzXngMxbS2Yufb-R-Nov6ry5uBzf8",
+
+  "authDomain": "login-b428a.firebaseapp.com",
+
+  "projectId": "login-b428a",
+
+  "storageBucket": "login-b428a.appspot.com",
+
+  "messagingSenderId": "653591106935",
+
+  "appId": "1:653591106935:web:5f88b914822571aa6e7d72",
+
+  "measurementId": "G-C89LKJP4T7",
+  "databaseURL" : ""
 }
 
+firebase = pyrebase.initialize_app(firebaseConfig)
+auth=firebase.auth()
+
+
+app = Flask(__name__, template_folder='templates', static_folder='static')
+app.config['SECRET_KEY'] = 'super-secret-key'
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -24,7 +37,36 @@ def signin():
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
+  if request.method == 'GET':
     return render_template("signup.html")
+  else:
+    login_session['email'] = request.form['email']
+    login_session['password'] = request.form['password']
+    email = login_session['email']
+    password = login_session['password'] 
+    
+    try:
+      login_session['user'] = auth.create_user_with_email_and_password(login_session['email'], login_session['password'])
+      return redirect(url_for('add_tweet'))
+    except:
+      error = "Authentication failed"
+      return error
+
+def signup():
+  if request.method == 'GET':
+    return render_template("signin.html")
+  else:
+    login_session['email'] = request.form['email']
+    login_session['password'] = request.form['password']
+    email = login_session['email']
+    password = login_session['password'] 
+    
+    try:
+      login_session['user'] = auth.sign_in_with_email_and_password(login_session['email'], login_session['password'])
+      return redirect(url_for('add_tweet'))
+    except:
+      error = "Authentication failed"
+      return error
 
 
 @app.route('/add_tweet', methods=['GET', 'POST'])
